9492f06410672ec0f642c00c7d9f347e2affea55
Abolfazl Davoodi Shandiz <a.davoodishandiz@live.com>
2024-04-23 17:21:08 +0330

    Add project files.

---
+++ BookStore.MvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BookStore.MvcUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
